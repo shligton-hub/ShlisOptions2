4a79f51aca2996e20efe51bb2b2420407de4afd5
shligton-hub <shligton@gmail.com>
2026-08-10 22:18:43 -0400

    Update Table 3 seating guests

--- wedding-seating/guests.js
+++ wedding-seating/guests.js
@@ -1,4 +1,11 @@
 window.SEATING_GUESTS = [
-  { first: "John", last: "Doe", table: "3" },
-  { first: "Jane", last: "Smith", table: "6" }
+  { first: "Cyndia", last: "Millender", table: "3" },
+  { first: "Marcus", last: "Millender", table: "3" },
+  { first: "Kress", last: "Estime", table: "3" },
+  { first: "Grazuela", last: "Pierre", table: "3" },
+  { first: "Lisa", last: "Estime", table: "3" },
+  { first: "Stevencia", last: "Estime", table: "3" },
+  { first: "Vernet (Tracy)", last: "Estime", table: "3" },
+  { first: "Alicia", last: "Estime", table: "3" },
+  { first: "Luzette", last: "Estime", table: "3" }
 ];
